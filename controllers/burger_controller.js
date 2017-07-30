12c4601eeff14544be2e66e61372122b61921f2e
Brain Huston <hbriandrums@gmail.com>
2017-07-29 20:24:16 -0700

    fixed controller, burger.js

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,10 +1,33 @@
 var express = require("express");
-
 var router = express.Router();
-
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    res.render("index", burger_data: data);
+  });
+});
 
+router.post("/", function(req, res) {
+  burger.create([
+    "burger_name"
+  ], [
+    req.body.burger_name
+  ], function() {
+    res.redirect("/");
+  });
+});
 
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,17 @@
 var orm = require("../config/orm.js");
 
-
-
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cb) {
+  	orm.create("burgers", ["burger_name", "devoured"], cb);
+  }
+  update: function(cb) {
+  	var burgerId = burgers.id;
+  	orm.update("burgers", devoured: true, burgerId);
+  } 	
+}
 module.exports = burger;
